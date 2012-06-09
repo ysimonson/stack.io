f02537d4eb85a245577e3a98c4862822f07f4837
ysimonson <me@yusufsimonson.com>
2012-06-08 17:08:57 -0700

    Script to force cleanup of database entries from a failed test run

---
+++ src/setup/force_cleanup.sh
@@ -0,0 +1,6 @@
+zerorpc tcp://127.0.0.1:27616 remove_user test0
+zerorpc tcp://127.0.0.1:27616 remove_user test1
+zerorpc tcp://127.0.0.1:27616 remove_user test2
+zerorpc tcp://127.0.0.1:27616 remove_group test0-group
+zerorpc tcp://127.0.0.1:27616 remove_group test1-group
+zerorpc tcp://127.0.0.1:27616 remove_group test2-group
