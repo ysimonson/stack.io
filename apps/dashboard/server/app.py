43cc758dc4c8265287e9b6861b74e8a13cdec40e
ysimonson <yusuf@dotcloud.com>
2012-07-16 10:42:32 -0700

    [BROKEN] Started work on a python client

---
+++ apps/dashboard/server/app.py
@@ -0,0 +1,32 @@
+from stackio import StackIO
+
+class PythonExample(object):
+    """ Various convenience methods to make things cooler. """
+
+    def add_man(self, sentence):
+        """ End a sentence with ", man!" to make it sound cooler, and
+        return the result. """
+        return sentence + ", man!"
+
+    def add_42(self, n):
+        """ Add 42 to an integer argument to make it cooler, and return the
+        result. """
+        return n + 42
+
+    def boat(self, sentence):
+        """ Replace a sentence with "I'm on a boat!", and return that,
+        because it's cooler. """
+        return "I'm on a boat!"
+
+def main():
+    client = StackIO()
+
+    print client.services()
+    print client.introspect("registrar")
+    registrar = client.use("registrar")
+    registrar.register("WTFISTHIS", "tcp://127.0.0.1:1111")
+
+    client.expose("example-python", "tcp://127.0.0.1:4243", PythonExample())
+
+if __name__ == "__main__":
+    main()
