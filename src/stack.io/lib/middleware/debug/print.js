545a5c1de7681380124dabd9990395dda2801d1f
ysimonson <yusuf@dotcloud.com>
2012-07-23 17:40:14 -0700

    Made all print messages colorful

--- src/stack.io/lib/middleware/debug/print.js
+++ src/stack.io/lib/middleware/debug/print.js
@@ -23,12 +23,16 @@
 
 var util = require("util");
 
+function inspect(obj) {
+    return util.inspect(obj, false, 4, true);
+}
+
 //Prints requests as the come in
 module.exports = function(req, res, next) {
-    console.log("REQUEST", util.inspect(req, false, 4, true));
+    console.log("REQUEST", inspect(req));
 
     res.on("update", function(error, result, more) {
-        console.log("UPDATE RESPONSE", error, result, more);
+        console.log("UPDATE RESPONSE", inspect(error), inspect(result), inspect(more));
     });
 
     next();
